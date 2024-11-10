674d43b8e7861a9cfb53964b40040d2042f6674f
Vadim <_s_e_t__@ukr.net>
2024-11-10 15:47:09 +0200

    add some code

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Header from "./components/Header.jsx";
 import Shop from "./components/Shop.jsx";
 import Product from "./components/Product.jsx";
 
+import { CartContext } from "./store/shopping-cart-context.jsx";
+
 import { DUMMY_PRODUCTS } from "./dummy-products.js";
 
 function App() {
@@ -67,8 +69,13 @@ function App() {
     });
   }
 
+  const ctxValue = {
+    items: shoppingCart.items,
+    addItemToCart: handleAddItemToCart,
+  }
+
   return (
-    <>
+    <CartContext.Provider value={ctxValue}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
@@ -80,7 +87,7 @@ function App() {
           </li>
         ))}
       </Shop>
-    </>
+    </CartContext.Provider>
   );
 }
 
